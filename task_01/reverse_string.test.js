6628d621a57ad4ba187e9384bd0c177fe2fb6748
Dmitrii Rychkov <dmitrii@rychkov.pro>
2024-07-17 18:43:58 +0500

    Examination

--- task_01/reverse_string.test.js
+++ task_01/reverse_string.test.js
@@ -1,14 +1,43 @@
 const reverseString = require('./task_01');
 
-test(
-    'Проверяем реверс строки',
+// test(
+//    'Проверяем реверс строки',
+//    () => {
+//        expect(reverseString('hello www')).toBe('www olleh');
+//    }
+//);
+//test(
+//    'Проверяем реверс строки',
+//    () => {
+//        expect(reverseString('test2')).toBe('2tset');
+//    }
+//);
+
+describe(
+    "Examination",
     () => {
-        expect(reverseString('hello www')).toBe('www olleh');
-    }
-);
-test(
-    'Проверяем реверс строки',
-    () => {
-        expect(reverseString('test2')).toBe('2tset');
-    }
-);
+    const testCase = [
+        {
+            inString: 'hello',
+            expected: 'olleh'
+        },
+        {
+            inString: 'test2',
+            expected: '2tset'
+        },
+        {
+            inString: '123456',
+            expected: '654321'
+        }
+    ];
+    testCase.forEach(test =>{
+        it (
+            `Входящая строка: ${test.inString} ожидаю: ${test.expected}`,
+            () => {
+                const res = reverseString(test.inString);
+                expect(res).toBe(test.expected);
+            }
+        );
+    });
+   } 
+); 
